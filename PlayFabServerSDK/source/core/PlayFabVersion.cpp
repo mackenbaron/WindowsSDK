d04f83777954137deb5960c40789ebad293ef8ac
jeremy-playfab <jeremy@playfab.com>
2014-09-22 13:47:55 -0700

    Updated to 1.0.0
    Includes new shared groups, simplified Google Play validation, item
    stores, and more!
    
    
    Former-commit-id: 8c14b7bbb208185564f8db9eec61c1b84611dc8d

--- PlayFabServerSDK/source/core/PlayFabVersion.cpp
+++ PlayFabServerSDK/source/core/PlayFabVersion.cpp
@@ -3,7 +3,7 @@
 
 namespace PlayFab
 {
-    const std::string PlayFabAPIRevision = "1.0.0.b8";
+    const std::string PlayFabAPIRevision = "1.0.0";
     const std::string PlayFabWindowsSDKVersion = "1.0.2";
-    const std::string PlayFabVersionString = "WindowsSDK-1.0.2-1.0.0.b8";
+    const std::string PlayFabVersionString = "WindowsSDK-1.0.2-1.0.0";
 }
