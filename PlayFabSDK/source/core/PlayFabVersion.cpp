e8ef01f8658002321d9b1f83f7237232e35dce9a
jeremy-playfab <jeremy@playfab.com>
2014-10-22 11:02:07 -0700

    1.0.2
    Added LoginWIthGameCenter
    
    
    Former-commit-id: 9eb89883a8fecd038fcdc4d01cc72aca8a31787c

--- PlayFabSDK/source/core/PlayFabVersion.cpp
+++ PlayFabSDK/source/core/PlayFabVersion.cpp
@@ -3,7 +3,7 @@
 
 namespace PlayFab
 {
-    const std::string PlayFabAPIRevision = "1.0.1";
+    const std::string PlayFabAPIRevision = "1.0.2";
     const std::string PlayFabWindowsSDKVersion = "1.0.2";
-    const std::string PlayFabVersionString = "WindowsSDK-1.0.2-1.0.1";
+    const std::string PlayFabVersionString = "WindowsSDK-1.0.2-1.0.2";
 }
