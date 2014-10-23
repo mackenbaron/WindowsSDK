fc81088517c38373c4c15337e162c887f6af2639
jeremy-playfab <jeremy@playfab.com>
2014-10-23 11:25:44 -0700

    Disabled gzip
    Gzipping the request body was causing problems with proxies. Disabled
    for now.
    
    
    Former-commit-id: 2fdc1e45c9aee0252541c4cba1a9931cb9889f99

--- PlayFabClientSDK/source/core/PlayFabVersion.cpp
+++ PlayFabClientSDK/source/core/PlayFabVersion.cpp
@@ -4,6 +4,6 @@
 namespace PlayFab
 {
     const std::string PlayFabAPIRevision = "1.0.2";
-    const std::string PlayFabWindowsSDKVersion = "1.0.2";
-    const std::string PlayFabVersionString = "WindowsSDK-1.0.2-1.0.2";
+    const std::string PlayFabWindowsSDKVersion = "1.0.3";
+    const std::string PlayFabVersionString = "WindowsSDK-1.0.3-1.0.2";
 }
