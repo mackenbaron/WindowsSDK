264a46483066cabc210ff4775a72211b9c66a07e
jeremy-playfab <jeremy@playfab.com>
2015-06-02 11:06:10 -0700

    Updated to 1.2.7
    Former-commit-id: b4b82c6f9bccf6079d986bb5e7d8885d9a4c2d42

--- PlayFabServerSDK/source/core/PlayFabVersion.cpp
+++ PlayFabServerSDK/source/core/PlayFabVersion.cpp
@@ -3,7 +3,7 @@
 
 namespace PlayFab
 {
-    const std::string PlayFabAPIRevision = "1.2.5";
+    const std::string PlayFabAPIRevision = "1.2.7";
     const std::string PlayFabWindowsSDKVersion = "1.0.3";
-    const std::string PlayFabVersionString = "WindowsSDK-1.0.3-1.2.5";
+    const std::string PlayFabVersionString = "WindowsSDK-1.0.3-1.2.7";
 }
