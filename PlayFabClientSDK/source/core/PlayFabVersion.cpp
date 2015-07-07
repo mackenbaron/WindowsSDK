2525b9f23aa403257a4f651151bb0b73ae1743e3
Calvin Kong <calvin@playfab.com>
2015-07-07 16:28:16 -0700

    snapshot 2015-07-07
    Former-commit-id: 966ec4e4abb38543a128d21ed81c506135805426

--- PlayFabClientSDK/source/core/PlayFabVersion.cpp
+++ PlayFabClientSDK/source/core/PlayFabVersion.cpp
@@ -3,7 +3,7 @@
 
 namespace PlayFab
 {
-    const std::string PlayFabAPIRevision = "1.3.20150618";
+    const std::string PlayFabAPIRevision = "1.3.20150707";
     const std::string PlayFabWindowsSDKVersion = "1.0.3";
-    const std::string PlayFabVersionString = "WindowsSDK-1.0.3-1.3.20150618";
+    const std::string PlayFabVersionString = "WindowsSDK-1.0.3-1.3.20150707";
 }
