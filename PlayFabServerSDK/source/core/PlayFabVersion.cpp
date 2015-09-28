41f4a27002ae912a49e5b4c8d30fec66c16f6123
pgilmorepf <paul@playfab.com>
2015-09-28 12:09:51 -0700

    Updating all SDKs after changes to API_Specs
    Former-commit-id: eeaba31420bd22c3ca04bb363a1e17c0d90c1f25

--- PlayFabServerSDK/source/core/PlayFabVersion.cpp
+++ PlayFabServerSDK/source/core/PlayFabVersion.cpp
@@ -3,7 +3,7 @@
 
 namespace PlayFab
 {
-    const std::string PlayFabAPIRevision = "1.5.20150901";
+    const std::string PlayFabAPIRevision = "1.6.20150928";
     const std::string PlayFabWindowsSDKVersion = "1.0.3";
-    const std::string PlayFabVersionString = "WindowsSDK-1.0.3-1.5.20150901";
+    const std::string PlayFabVersionString = "WindowsSDK-1.0.3-1.6.20150928";
 }
