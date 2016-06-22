7f36476e76fca513b854d00194264983e6c8797b
pgilmorepf <paul@playfab.com>
2016-06-21 17:40:08 -0700

    Deleting unused files.
    Former-commit-id: 31e0662f4f2cfd0bbb98147b7141a7d6017730d8

--- PlayFabClientSDK/source/core/PlayFabVersion.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "playfab/PlayFabVersion.h"
-
-namespace PlayFab
-{
-    const std::string PlayFabSDKVersion = "0.27.160606";
-    const std::string PlayFabVersionString = "WindowsSDK-0.27.160606";
-}
